2a6df851b09b7ab5300e98ec8a8b7ca732118c9e
tranvuluan2270 <tranvuluan2270@gmail.com>
2023-06-29 18:05:44 +0700

    navbar + react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,44 @@ import Home from "./components/Home";
 import AddNewProduct from "./components/AddNewProduct";
 import Product from "./components/Products/Product";
 import "react-image-lightbox/style.css";
+import Nav from "./components/Navigation/Nav";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header content-left">
-        <div style={{ textAlign: "center" }}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <Home />
-      </header>
-      <div className="content-right">
-        <AddNewProduct />
-        <hr />
-        <Product />
-      </div>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <div className="App">
+            <header className="App-header content-left">
+              <div style={{ textAlign: "center" }}>
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+              <Home />
+            </header>
+            <div className="content-right">
+              <AddNewProduct />
+              <hr />
+              <Product />
+            </div>
+          </div>
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+        <Route path="/weather">
+          <div>Weather App</div>
+        </Route>
+        <Route path="/about">
+          <div>About</div>
+        </Route>
+        <Route path="*">
+          <div>404 Not Found!</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
